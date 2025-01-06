88c69bb674b0ad39ab65a0639793ca4bf2897fbc
saiMulagada <sai.star47@gmail.com>
2025-01-06 21:25:03 +0530

    initial commit

--- core/settings.py
+++ core/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     "corsheaders.middleware.CorsMiddleware",
+     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
